cd6eefff54d3b1806e00f63158903a71d430a0b8
kamalboroKB <swarang.boro@gmail.com>
2025-09-04 18:03:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const toggle = document.querySelector(".toggle");
+const subMenu =  document.querySelector(".sub_menu")
+
+
+toggle.addEventListener("click", function(){
+    
+   if(subMenu.style.display == 'none'){
+
+        subMenu.style.display = "block";
+
+   }
+   else{
+    subMenu.style.display = "none";
+   }
+})
+
+document.addEventListener("click", function(event){
+    console.log(event.target);
+    console.log(subMenu)
+    
+
+    if(event.target !== toggle ){
+
+        
+        if(subMenu.style.display == "block"){
+                subMenu.style.display = "none";
+        }
+    }
+
+
+}
+)
