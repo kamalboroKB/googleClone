7edadea2edc1b7c2a9178407f90fb2051f35dc65
kamalboroKB <swarang.boro@gmail.com>
2025-09-05 19:08:56 +0530

    major_update_googleClone

--- index.js
+++ index.js
@@ -1,7 +1,129 @@
 
-// const toggle = document.querySelector(".toggle");
-// const subMenu =  document.querySelector(".sub_menu")
- 
+const toggle = document.querySelector(".toggle");
+
+const setTogg = document.querySelector(".setting");
+const set_menu = document.querySelector(".setting_menu");
+
+
+const subSetting = document.querySelector("#sub-setting")
+
+function toggleMenu(){
+    subSetting.classList.toggle("open-menu");
+}
+
+
+
+const subMenu =  document.querySelector("#sub_menu_id");
+
+function toggleMenu_sub(){
+    subMenu.classList.toggle("open-sub");
+}
+
+
+
+
+// const english = document.querySelector(".lang_english");
+// const hindi = document.querySelector(".lang_hindi");
+// bangla
+// telugu
+// marathi
+// tamil
+// gujrati
+// kannada
+// malayalam
+// Punjabi
+
+
+
+function english(){
+
+    document.querySelector("#language_text").innerHTML = "I'm Feeling Lucky";
+    
+    document.querySelector("#offer_text").innerHTML = "Google offered in";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_english").style.display = 'none';
+
+}
+
+
+function hindi(){
+
+    document.querySelector("#language_text").innerHTML = "आज मेरी किस्मत अच्छी है";
+    document.querySelector("#offer_text").innerHTML = "Google का इस्तेमाल इन भाषाओं में किया जा सकता है";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+
+function bangla(){
+
+    document.querySelector("#language_text").innerHTML = "ভাগ্যবান অনুভব করছি";
+    document.querySelector("#offer_text").innerHTML = "Google এই ভাষায় উপলব্ধ";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+function telugu(){
+
+    document.querySelector("#language_text").innerHTML = "నేను అదృష్టశాలి";
+    document.querySelector("#offer_text").innerHTML = "Google ఈ భాషల్లో అందుబాటులో ఉంది";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+function marathi(){
+
+    document.querySelector("#language_text").innerHTML = "मी भाग्यवान";
+    document.querySelector("#offer_text").innerHTML = "Google उपलब्ध आहे यामध्ये";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+function tamil(){
+
+    document.querySelector("#language_text").innerHTML = "அதிர்ஷ்டம் என் பக்கம்";
+    document.querySelector("#offer_text").innerHTML = "Google இதில் வழங்குகிறது";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+function gujrati(){
+
+    document.querySelector("#language_text").innerHTML = "મારું નસીબ જોર કરે છે";
+    document.querySelector("#offer_text").innerHTML = "Google આ ભાષાઓમાં ઉપલબ્ધ છે";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+function kannada(){
+
+    document.querySelector("#language_text").innerHTML = "ನಾನು ಅದೃಷ್ಟಶಾಲಿ";
+    document.querySelector("#offer_text").innerHTML = "Google ಇದರಲ್ಲಿ ಲಭ್ಯ";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+function malayalam(){
+
+    document.querySelector("#language_text").innerHTML = "എനിക്ക് ഭാഗ്യമുണ്ടെന്ന് തോന്നുന്നു";
+    document.querySelector("#offer_text").innerHTML = "Google ലഭ്യമായ ഭാഷകൾ";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+function punjabi(){
+
+    document.querySelector("#language_text").innerHTML = "ਮੈਂ ਖੁਸ਼ਕਿਸਮਤ ਹਾਂ";
+    document.querySelector("#offer_text").innerHTML = "ਇਹਨਾਂ ਵਿੱਚ Google ਦੀ ਪੇਸ਼ਕਸ਼ ਕੀਤੀ ਜਾਂਦੀ ਹੈ:  ";
+    document.querySelector(".all_language").style.display = 'inline-block';
+    // document.querySelector(".lang_hindi").style.display = 'none';
+
+}
+
+
+
+
+
 
 // toggle.addEventListener("click", function(){
     
@@ -13,21 +135,29 @@
 //    else{
 //     subMenu.style.display = "none";
 //    }
+   
 // })
 
-// document.addEventListener("click", function(event){
-//     console.log(event.target);
-//     console.log(subMenu)
-    
 
-//     if(event.target !== toggle ){
+// setTogg.addEventListener("click", function(){
 
-        
-//         if(subMenu.style.display == "block"){
-//                 subMenu.style.display = "none";
-//         }
+//     if(set_menu.style.display == 'none'){
+
+//         set_menu.style.display = 'block';
+//     }
+//     else{
+//         set_menu.style.display = 'none';
 //     }
 
+    
+// })
 
-// }
-// )
+
+
+
+
+
+
+
+
+
